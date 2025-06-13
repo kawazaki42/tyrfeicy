8c708907e0ddc086782843b911e862535cac2a9e
anonymous <>
2025-06-13 17:27:03 +0900

    Added Dynlib example

---
+++ DynlibExample/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int add_numbers(int, int);
+void print_message(const char *message);
+
+int main() {
+	// printf("%s\n", "Hello, World!");
+	printf("%d bratyxa", add_numbers(40, 2) );
+	putchar('\n');
+
+	print_message("The cake is a lie");
+}
---
+++ DynlibExample/my.pas
@@ -0,0 +1,38 @@
+library my;
+
+{ function add_numbers(a, b: Integer): Integer; }
+
+function add_numbers(a, b: Integer): Integer;
+begin
+  add_numbers := a + b;
+end;
+
+
+{ procedure print_message(const message: PChar); }
+{ { cdecl; } }
+{ begin }
+{   writeln('Message: ', message); }
+{ end; }
+
+procedure print_message(const message: array of Char);
+var
+  c: Char;
+{ cdecl; }
+begin
+  write('Message: ');
+  
+  for c in message do
+  begin
+    if c = chr(0) then
+      break;
+      
+    write(c);
+  end;
+
+  writeln;
+end;
+
+exports
+  add_numbers, print_message;
+
+end.
