a212bb4a9b2c9848fa41411c50f7f57c22f4808c
anonymous <>
2025-06-13 18:55:57 +0900

    Documented code in DynlibExample

--- DynlibExample/main.c
+++ DynlibExample/main.c
@@ -1,12 +1,22 @@
+/* Программа на языке C, использующая бибоиотеку на Паскале. */
+
+/* Стандартный ввод/вывод */
 #include <stdio.h>
 
+/* Объявления внешних подпрограмм */
+
+/* Сложить два целых числа */
 int add_numbers(int, int);
+/* Вывести сообщение message, начиная строку с 'Message: ' */
 void print_message(const char *message);
 
+/* Главная программа */
 int main() {
-	// printf("%s\n", "Hello, World!");
+    /* Тест функции add_numbers. Должен отобразить '42 bratyxa' */
 	printf("%d bratyxa", add_numbers(40, 2) );
-	putchar('\n');
+	putchar('\n'); /* Перевод строки */
 
+	/* Тест функции (процедуры) print_message.
+	   Должен отобразить 'Message: The cake is a lie' */
 	print_message("The cake is a lie");
 }
--- DynlibExample/my.pas
+++ DynlibExample/my.pas
@@ -1,18 +1,43 @@
+// Библиотека на Паскале.
+// Пример использования приведен на языке C в main.c
 library my;
+// NOTE: ключевое слово library вместо unit
 
+// Реализации подпрограмм.
+// NOTE: не работают ключевые слова interface, implementation.
+
+// NOTE: ключевое слово cdecl устанавливает C-подобный стиль объявлений.
+//       Для использования в других программах он должен совпадать!
+//       cdecl - самый распространенный стиль.
+
+
+// Сложить целые числа a и b.
 function add_numbers(a, b: Integer): Integer;
+cdecl;
 begin
   add_numbers := a + b;
 end;
 
-
+// Вывести сообщение message.
 procedure print_message(const message: PChar);
+// NOTE: PChar - тип строки в стиле C.
+//
+//       Представляет собой указатель на начало
+//       непрерывной последовательности символов.
+//
+//       Конец строки обозначается нуль-терминатором
+//       т.е. `Chr(0)`, или же `#0`
+//
+//       С обычным String работает только сам Pascal.
 cdecl;
 begin
-  writeln('Message: ', message);
+  writeln('Message: ', message);  // Pascal умеет работать с PChar
 end;
 
 {
+// Такая реализация тоже работает!
+
+// Вывести сообщение message.
 procedure print_message(const message: array of Char);
 var
   c: Char;
@@ -22,7 +47,7 @@ begin
   
   for c in message do
   begin
-    if c = chr(0) then
+    if c = #0 then
       break;
       
     write(c);
@@ -32,6 +57,8 @@ begin
 end;
 }
 
+// NOTE: exports вместо interface;
+//       должен быть ПОСЛЕ реализации.
 exports
   add_numbers, print_message;
 
