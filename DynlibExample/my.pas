c043ee7928a54a00a8e75430f19b1131fffb1989
anonymous <>
2025-06-13 17:31:20 +0900

    keep the saner PChar example

--- DynlibExample/my.pas
+++ DynlibExample/my.pas
@@ -1,19 +1,18 @@
 library my;
 
-{ function add_numbers(a, b: Integer): Integer; }
-
 function add_numbers(a, b: Integer): Integer;
 begin
   add_numbers := a + b;
 end;
 
 
-{ procedure print_message(const message: PChar); }
-{ { cdecl; } }
-{ begin }
-{   writeln('Message: ', message); }
-{ end; }
+procedure print_message(const message: PChar);
+{ cdecl; }
+begin
+  writeln('Message: ', message);
+end;
 
+{
 procedure print_message(const message: array of Char);
 var
   c: Char;
@@ -31,6 +30,7 @@ begin
 
   writeln;
 end;
+}
 
 exports
   add_numbers, print_message;
